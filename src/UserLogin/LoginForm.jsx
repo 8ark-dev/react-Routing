9214d98436fd964ecb5f5d1fd3b32a6abfa918da
8ark-dev <sorlti6952@gmail.com>
2023-01-17 09:56:21 +0900

    DOCS LoginForm init

--- src/UserLogin/LoginForm.jsx
+++ src/UserLogin/LoginForm.jsx
@@ -2,8 +2,47 @@ import React, {useRef} from 'react'
 import {useHistory} from "react-router-dom";
 
 function LoginForm() {
+  const history = useHistory();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+  };
+
   return (
-    <div>LoginForm</div>
+    <div>
+      <form>
+        <fieldset>
+          <label htmlFor='email'>Email</label>
+          <input
+            placeholder='enter email.'
+            required
+            ref={emailRef}
+            id="email"
+            type="email"
+            name='email'
+            autoComplete='off'
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <input
+            placeholder='enter password.'
+            required
+            id='password'
+            name='password'
+            type='password'
+          />
+        </fieldset>
+        <button type='submit' onClick={submitForm}>
+          Login
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/UserLogin/UserLogin.jsx
+++ src/UserLogin/UserLogin.jsx
@@ -13,4 +13,44 @@ function UserLogin() {
   )
 }
 
+function HomePage() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <div>
+        {}
+      </div>
+    </div>
+  );
+}
+
+function LoginPage() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <LoginForm/>
+      <div>
+        {}
+      </div>
+    </div>
+  );
+}
+
+function UserDetailPage() {
+  const email = 'abc@def.com'
+  const password = '1234'
+
+  return (
+    <div>
+      <h2>User Detail Page</h2>
+      <p>
+        <h3>User Details</h3>
+        <em>{email}</em>
+        <br/>
+        <strong>{password}</strong>
+      </p>
+      {}
+    </div>
+  );
+}
 export default UserLogin
